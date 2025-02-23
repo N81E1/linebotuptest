52a3a072e1c30572667722ca7429c17fd65cd9b5
N81E1 <102803191+N81E1@users.noreply.github.com>
2025-02-23 19:57:26 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,60 +1,52 @@
+#載入LineBot所需要的模組
 from flask import Flask, request, abort
-
-from linebot.v3 import (
-    WebhookHandler
+ 
+from linebot import (
+    LineBotApi, WebhookHandler
 )
-from linebot.v3.exceptions import (
+from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage
-)
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent
-)
-import os
-
+from linebot.models import *
 
 app = Flask(__name__)
+ 
+# 必須放上自己的Channel Access Token
+line_bot_api = LineBotApi('D7W0ZAHVeD47r30IhGN2DTmcUJdMV9hI6JhOvH6VJUTHsW3nkwjJEe304TDV4ZwrEzUFAj/Fg2hVCd6/fcxKaiALt0YMr34U7ZNDRthl8UIOaBTR/vC05rrfabNL3I4tf9O0MBQlnM3Ckqtd/VTKCwdB04t89/1O/w1cDnyilFU=')
+ 
+# 必須放上自己的Channel Secret
+handler = WebhookHandler('60e4d735d45b63eeb9c0713abc80c9ce')
 
-configuration = Configuration(access_token=os.getenv('CHANNEL_ACCESS_TOKEN'))
-line_handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+line_bot_api.push_message('U328d06982248f7b955f81193d2084da6', TextSendMessage(text='你可以開始了'))
 
-
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
+ 
+  
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
+ 
     # handle webhook body
     try:
-        line_handler.handle(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
+ 
     return 'OK'
 
-
-@line_handler.add(MessageEvent, message=TextMessageContent)
+    #訊息傳遞區塊
+##### 基本上程式編輯都在這個function #####
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
-            )
-        )
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
 
+#主程式
+import os
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
